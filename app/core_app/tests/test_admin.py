b7c340f56928859817c4c9202600105a025721ca
litovchenko <y.litovchenko@hicell.tel>
2022-02-02 19:22:56 +0200

    lint

--- app/core_app/tests/test_admin.py
+++ app/core_app/tests/test_admin.py
@@ -32,7 +32,7 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
 
-    def test_create_user_pafe(self):
+    def test_create_user_page(self):
         """Test that create user page works"""
         url = reverse('admin:core_app_user_add')
         res = self.client.get(url)
