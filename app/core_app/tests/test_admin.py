c73394bff285c1efde866664ff34c4ca725ea09c
litovchenko <y.litovchenko@hicell.tel>
2022-02-02 18:38:14 +0200

    Updated admin to support user custom model

--- app/core_app/tests/test_admin.py
+++ app/core_app/tests/test_admin.py
@@ -31,3 +31,9 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_app_user_change', args=[self.user.id])
         res = self.client.get(url)
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_pafe(self):
+        """Test that create user page works"""
+        url = reverse('admin:core_app_user_add')
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
